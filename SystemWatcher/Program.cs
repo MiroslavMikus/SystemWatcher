83daec082608bf94a33fea3453a6d3d518650e49
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2019-04-10 21:49:50 +0200

    Add project files.

---
+++ SystemWatcher/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace SystemWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(a =>
+            {
+                a.Service<Watcher>(b =>
+                {
+                    b.ConstructUsing(c => new Watcher());
+                    b.WhenStarted(c => c.Start());
+                    b.WhenStopped(c => c.Stop());
+                });
+
+                a.RunAsLocalSystem();
+
+                a.SetServiceName("System watcher");
+                a.SetDisplayName("System watcher");
+            });
+
+            Environment.ExitCode = (int)exitCode;
+        }
+    }
+}
---
+++ SystemWatcher/Watcher.cs
@@ -0,0 +1,59 @@
+﻿using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SystemWatcher
+{
+    public class Watcher
+    {
+        CancellationTokenSource _cancellation;
+
+        public void Start()
+        {
+            _cancellation = new CancellationTokenSource();
+            CreateLogger();
+
+            Task.Run(async () =>
+            {
+                var memoryCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+                var cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+
+                while (true)
+                {
+                    if (_cancellation.Token.IsCancellationRequested) break;
+
+                    await Task.Delay(1000);
+
+                    // read cpu
+                    Log.Logger.Information("Cpu usage {usage}", cpuCounter.NextValue());
+
+                    // read ram
+                    Log.Logger.Information("Ram usage {usage}", memoryCounter.NextValue());
+                }
+            });
+        }
+
+        public void Stop()
+        {
+            _cancellation.Cancel();
+        }
+
+        private static void CreateLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                            .Enrich.WithProperty("system", "Miro-Laptop")
+                            .Enrich.FromLogContext()
+                            .MinimumLevel.Debug()
+                            .WriteTo.Console()
+                            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+                            {
+                                AutoRegisterTemplate = true,
+                                AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
+                            })
+                            .CreateLogger();
+        }
+    }
+}
